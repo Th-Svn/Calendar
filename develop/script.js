0808b6c452c12660ffca3661f31eb8367c5cdcd1
Th-Svn <thelma.sevenier14@gmail.com>
2021-04-25 23:26:52 -0500

    create a save tasks option with js

--- develop/script.js
+++ develop/script.js
@@ -3,8 +3,18 @@
  var date=(today.getMonth()+1) + ' - ' + today.getDate() + ' - ' + today.getFullYear();
  document.getElementById("p1").innerHTML = date;
 
- $(document).ready(function(){
-    $(".btn").click(function(){
-        $("#myModal").modal('show');
-    });
-});
+
+$("button").on("click", function() {
+    event.preventDefault();
+    var container = $(this).parent().parent();  
+    var inputValue = container.find("input").val();
+    var inputId = container.find("input").attr("id");
+    var textObj = {
+      "input-id": inputId,
+      "input-value": inputValue };
+    
+    if (textObj["input-value"] !== "") {
+      plannerContent.push(textObj);
+      localStorage.setItem("planner-items", JSON.stringify(plannerContent));
+    }
+  });
